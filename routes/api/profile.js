7647996637c4915929ffb17861f7dbfd7ac161f7
Dan Collins <32271799+dacogemini@users.noreply.github.com>
2018-05-09 12:45:40 -0500

    Added validation

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,81 +1,152 @@
 // Location, Bio, Experience, Education, Social Links
-
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const passport = require('passport');
+
+// Load Validation
+const validateProfileInput = require('../../validation/profile');
 // Load Profile Model
 const Profile = require('../../models/Profile');
-// Load Profile Model
+// Load User Model
 const User = require('../../models/User');
 
-// @route GET api/profile/test
-// @desc Tests profile route
-// @access public
+// @route   GET api/profile/test
+// @desc    Tests profile route
+// @access  Public
+router.get('/test', (req, res) => res.json({ msg: 'Profile Works' }));
 
-router.get('/test', (req, res) => res.json({ //<<< Serves JSON
-    msg: "Profile Works"
-}));
-
-// @route GET api/profile
-// @desc Get current users profile
-// @access private
-
-router.get('/', passport.authenticate('jwt', { session: false }), (req, res) => {
+// @route   GET api/profile
+// @desc    Get current users profile
+// @access  Private
+router.get(
+  '/',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
     const errors = {};
+
     Profile.findOne({ user: req.user.id })
-        .then(profile => {
-            if(!profile) {
-                errors.noprofile = 'There is not a profile for this user';
-                return res.status(404).json(errors);
-            }
-            res.json(profile);
-        })
-        .catch(err => res.status(404).json(err));
+      .populate('user', ['name', 'avatar'])
+      .then(profile => {
+        if (!profile) {
+          errors.noprofile = 'There is no profile for this user';
+          return res.status(404).json(errors);
+        }
+        res.json(profile);
+      })
+      .catch(err => res.status(404).json(err));
+  }
+);
+
+// @route   GET api/profile/all
+// @desc    Get all profiles
+// @access  Public
+router.get('/all', (req, res) => {
+  const errors = {};
+
+  Profile.find()
+    .populate('user', ['name', 'avatar'])
+    .then(profiles => {
+      if (!profiles) {
+        errors.noprofile = 'There are no profiles';
+        return res.status(404).json(errors);
+      }
+
+      res.json(profiles);
+    })
+    .catch(err => res.status(404).json({ profile: 'There are no profiles' }));
 });
 
-// @route POST api/profile
-// @desc create user profile
-// @access private
+// @route   GET api/profile/handle/:handle
+// @desc    Get profile by handle
+// @access  Public
+
+router.get('/handle/:handle', (req, res) => {
+  const errors = {};
+
+  Profile.findOne({ handle: req.params.handle })
+    .populate('user', ['name', 'avatar'])
+    .then(profile => {
+      if (!profile) {
+        errors.noprofile = 'There is no profile for this user';
+        res.status(404).json(errors);
+      }
+
+      res.json(profile);
+    })
+    .catch(err => res.status(404).json(err));
+});
+
+// @route   GET api/profile/user/:user_id
+// @desc    Get profile by user ID
+// @access  Public
+
+router.get('/user/:user_id', (req, res) => {
+  const errors = {};
+
+  Profile.findOne({ user: req.params.user_id })
+    .populate('user', ['name', 'avatar'])
+    .then(profile => {
+      if (!profile) {
+        errors.noprofile = 'There is no profile for this user';
+        res.status(404).json(errors);
+      }
+
+      res.json(profile);
+    })
+    .catch(err =>
+      res.status(404).json({ profile: 'There is no profile for this user' })
+    );
+});
+
+// @route   POST api/profile
+// @desc    Create or edit user profile
+// @access  Private
 
 router.post(
-    '/',
-    passport.authenticate('jwt', { session: false }),
-    (req, res) => 
-    {   // Get fields
-        const profileFields = {}; //<<< Info from the form
-        profileFields.user = req.user.id //<<< .user does NOT come from the form
-        if(req.body.handle) profileFields.handle = req.body.handle; //<<< Check if the field we are looking for has come in
-        if(req.body.company) profileFields.company = req.body.company; //<<< Check if the field we are looking for has come in
-        if(req.body.website) profileFields.website = req.body.website; //<<< Check if the field we are looking for has come in
-        if(req.body.bio) profileFields.bio = req.body.bio; //<<< Check if the field we are looking for has come in
-        if(req.body.status) profileFields.status = req.body.status; //<<< Check if the field we are looking for has come in
-        if(req.body.githubusername) profileFields.githubusername = req.body.githubusername; //<<< Check if the field we are looking for has come in
-        // Skills - Split into an array
-        if(typeof req.body.skills !== 'undefined') {
-            profileFields.skills = req.body.skills.split(',')
-        } 
-        // Social 
-        profileFields.social = {}; // Initialize
-        // Comes in as this  // if exists...                // Set to this value                
-        if(req.body.youtube) profileFields.social.youtube = req.body.youtube; //<<< Check if the field we are looking for has come in
-        if(req.body.twitter) profileFields.social.twitter = req.body.twitter; //<<< Check if the field we are looking for has come in
-        if(req.body.facebook) profileFields.social.facebook = req.body.facebook; //<<< Check if the field we are looking for has come in
-        if(req.body.linkedin) profileFields.social.linkedin = req.body.linkedin; //<<< Check if the field we are looking for has come in
-        if(req.body.instagram) profileFields.social.instagram = req.body.instagram; //<<< Check if the field we are looking for has come in
+  '/',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validateProfileInput(req.body);
 
-        // ─── SEARCH FOR USER BY LOGGED IN ID ─────────────────────────────
+    // Check Validation
+    if (!isValid) {
+      // Return any errors with 400 status
+      return res.status(400).json(errors);
+    }
 
-        Profile.findOne({ user: req.user.id })
-            .then(profile => {
-                if(profile) {
-                    // IF USER HAS PROFILE UPDATE HERE v
-                    Profile.findOneAndUpdate(
-                        { user: req.user.id }, 
-                        { $set: profileFields }, 
-                        { new: true }
-                    )
-                    .then(profile => res(profile));
+    // Get fields
+    const profileFields = {};
+    profileFields.user = req.user.id;
+    if (req.body.handle) profileFields.handle = req.body.handle;
+    if (req.body.company) profileFields.company = req.body.company;
+    if (req.body.website) profileFields.website = req.body.website;
+    if (req.body.location) profileFields.location = req.body.location;
+    if (req.body.bio) profileFields.bio = req.body.bio;
+    if (req.body.status) profileFields.status = req.body.status;
+    if (req.body.githubusername)
+      profileFields.githubusername = req.body.githubusername;
+    // Skills - Spilt into array
+    if (typeof req.body.skills !== 'undefined') {
+      profileFields.skills = req.body.skills.split(',');
+    }
+
+    // Social
+    profileFields.social = {};
+    if (req.body.youtube) profileFields.social.youtube = req.body.youtube;
+    if (req.body.twitter) profileFields.social.twitter = req.body.twitter;
+    if (req.body.facebook) profileFields.social.facebook = req.body.facebook;
+    if (req.body.linkedin) profileFields.social.linkedin = req.body.linkedin;
+    if (req.body.instagram) profileFields.social.instagram = req.body.instagram;
+
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      if (profile) {
+        // Update
+        Profile.findOneAndUpdate(
+          { user: req.user.id },
+          { $set: profileFields },
+          { new: true }
+        ).then(profile => res.json(profile));
                     // IF USER HAS PROFILE UPDATE HERE ^
                 } 
                 else 
